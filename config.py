986cbd28003769b7f085d7012df49e022931f0d1
priyesh2021 <priyesh1167.be20@chitkarauniversity.edu.in>
2025-03-13 13:21:17 +0530

    add config.py for DB_CREDENTIALS

---
+++ config.py
@@ -0,0 +1,11 @@
+
+class Config:
+    
+    DB_HOST = "localhost"  # Replace with your database host
+    DB_PORT = 3306         # Replace with your database port
+    DB_USER = "root"       # Replace with your database username
+    DB_PASSWORD = ""       # Replace with your database password
+    DB_NAME = "file_manager"  # Replace with your database name
+
+
+config = Config()
